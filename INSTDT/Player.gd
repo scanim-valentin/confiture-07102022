f38a49921669b5c0efb2eb04d6704c540020e53f
Newg <g_robert@insa-toulouse.fr>
2022-10-08 10:59:17 +0200

    Ajout des animations du joeurs et sprite de la bombe

--- INSTDT/Player.gd
+++ INSTDT/Player.gd
@@ -1,48 +1,56 @@
 extends Area2D
 
 export var speed= 100.0;
-var bombDist = 2;
+export var bombDist = 2;
+
+var anim: AnimatedSprite; 
 var screen_size;
 var  direction;
-var sp
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	anim = get_node("AnimatedSprite")
 	direction = Vector2(1,0)
-	sp = get_node("Sprite")
 	screen_size = get_viewport_rect().size
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	_move(delta);
-	_flip()
+	if not anim.is_playing():
+		anim.play()
 	if Input.is_action_just_pressed("Put_Bomb"):
 		_put_Bomb()
+	print(direction)
 
-func _flip():
-	if direction.x > 0:
-		sp.scale.x = 1 
-	else:
-		sp.scale.x = -1
-	if direction.y > 0: 
-		sp.scale.y = 1
-	else:
-		sp.scale.y = -1
 	
 func _move(delta):
 	var velocity = Vector2.ZERO
-	
 	if(Input.is_action_pressed("move_up")):
-			direction =	Vector2(direction.x,-1)
+			direction =	Vector2(0,-1)
+			anim.animation = "Walk_Up"
 			velocity.y -= speed
-	if(Input.is_action_pressed("move_down")):
-			direction =	Vector2(direction.x,1)
+	elif(Input.is_action_pressed("move_down")):
+			direction =	Vector2(0,1)
+			anim.animation = "Walk_Down"
 			velocity.y += speed
-	if(Input.is_action_pressed("move_left")):
-			direction =	Vector2(-1,direction.y)
+	elif(Input.is_action_pressed("move_left")):
+			direction =	Vector2(-1,0)
+			anim.animation = "Walk_Left"
 			velocity.x -= speed
-	if(Input.is_action_pressed("move_right")):
-			direction =	Vector2(1,direction.y)
+	elif(Input.is_action_pressed("move_right")):
+			direction =	Vector2(1,0)
+			anim.animation = "Walk_Right"
 			velocity.x += speed
+	else:
+		if direction.x == -1: 
+			anim.animation = "Idle_Left"
+		elif direction.x == 1:
+			anim.animation = "Idle_Right"
+		elif direction.y == -1:
+			anim.animation = "Idle_Up"
+		elif direction.y == 1:
+			anim.animation = "Idle_Down"
+
 	position += velocity*delta
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
@@ -50,4 +58,4 @@ func _move(delta):
 func _put_Bomb():
 		var bomb = load("res://Bomb.tscn").instance()
 		get_tree().get_root().add_child(bomb)
-		bomb.position = position * direction + bombDist * direction
+		bomb.position = position + bombDist * direction

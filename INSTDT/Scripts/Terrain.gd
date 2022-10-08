c731fb4fa96033d744df0f15cf927a24b535fc45
scanim-valentin <guerbett@etud.insa-toulouse.fr>
2022-10-08 15:15:53 +0200

    bloc effects

--- INSTDT/Scripts/Terrain.gd
+++ INSTDT/Scripts/Terrain.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-# Terrain description using characters. " " : empty ; "#" : wall (unbreakable)
+# Terrain description using characters.
 var terrainArray = [
 "  #####################",
 "      t   *           #",
@@ -32,22 +32,48 @@ export(PackedScene) var flower
 export(PackedScene) var tree
 export(PackedScene) var dust
 
-#Useless for now
-class BlocEffects :
-	var Damage: int
-	var ExplosionRange: int
 
 var spriteSize = 48 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
-	var generationDict = {"#": wall, "*":flower, "o":dust, "t":tree, "~": breakable_wall} 
+	var effectArray = [
+		{
+	"damage" : 1,
+	"explosionRange" : 1,
+	"destruction": true
+	},
+	{
+	"damage" : 2,
+	"explosionRange" : 0,
+	"destruction": true
+	},
+	{
+	"damage" : -1,
+	"explosionRange" : 0,
+	"destruction": true 
+	},
+	{
+	"damage" : -2,
+	"explosionRange" : 2,
+	"destruction": true 
+	}]
+	effectArray.shuffle()
+	var generationDict = {"#": {"bloc": wall,           "effect": {"damage": 0, 
+																   "explosionRange": 0, 
+																   "destruction": false}},
+						  "*": {"bloc": flower,         "effect":effectArray[0]}, 
+						  "o": {"bloc": dust,           "effect":effectArray[1]}, 
+						  "t": {"bloc": tree,           "effect":effectArray[2]}, 
+						  "~": {"bloc": breakable_wall, "effect":effectArray[3]}} 
+	
 	#Generating terrain	
 	for i in range(terrainArray.size()) :
 		for j in terrainArray[i].length() :
 			if(terrainArray[i][j] != " ") :
-				#print(generationDict[ terrainArray[i][j] ]) 
-				var newwall = generationDict[ terrainArray[i][j] ].instance()
+#				print(generationDict[ terrainArray[i][j] ]["bloc"]) 
+				var newwall = generationDict[ terrainArray[i][j] ]["bloc"].instance()
+				newwall.effect = generationDict[ terrainArray[i][j] ]["effect"]
 				newwall.position = Vector2(j*spriteSize,i*spriteSize)
 				add_child(newwall)
 					
--- INSTDT/Scripts/TerrainElement.gd
+++ INSTDT/Scripts/TerrainElement.gd
@@ -5,14 +5,18 @@ extends Node2D
 # var a = 2
 # var b = "text"
 
-export(int) var damage
-export(int) var perk
-export(int) var explosion_area
+export(Dictionary) var effect
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func _onExplosion():
+	
+	#Destruction upon explosion
+	if(effect["destruction"]):
+		hide()
+		queue_free()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):

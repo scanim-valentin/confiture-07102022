0090151332fe69a124f371396f4f5b1e7dbfb2d0
scanim-valentin <guerbett@etud.insa-toulouse.fr>
2022-10-08 12:00:04 +0200

    generation

--- INSTDT/Scripts/Terrain.gd
+++ INSTDT/Scripts/Terrain.gd
@@ -3,22 +3,22 @@ extends Node2D
 # Terrain description using characters. " " : empty ; "#" : wall (unbreakable)
 var terrainArray = [
 "  #####################",
-"  # # #   #           #",
+"  # # #   *           #",
 "# # # ### ### ### ### #",
-"# #   #   # # #     # #",
+"# #   #   * # #     # #",
 "# ### # ### ### ### ###",
-"#   #   #   #   #     #",
+"#   #   *   #   #     #",
 "# ##### ### # # ### ###",
 "#           # #   #   #",
 "### # ##### # ### ### #",
-"#   #     # #   # #   #",
+"#   #     * #   # #   #",
 "# # # ####### # # #####",
 "# # #   # # # # # #   #",
 "# ####### # # ### # ###",
 "#       # #   #   #   #",
 "# ### # # # ##### ### #",
 "#   # # #   #     #   #",
-"### # ########### ### #",
+"### # #######*### ### #",
 "#   # # # #     #     #",
 "# ##### # # ####### # #",
 "# #         #     # # #",
@@ -27,6 +27,12 @@ var terrainArray = [
 "#####################  " ]
 
 export(PackedScene) var wall
+export(PackedScene) var breakable_wall
+export(PackedScene) var flower
+export(PackedScene) var plant2
+export(PackedScene) var plant3
+export(PackedScene) var dust
+
 var spriteSize = 24 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -35,6 +41,10 @@ func _ready():
 			match terrainArray[i][j] : 
 				'#':
 					var newwall = wall.instance()
+					newwall.position = Vector2(j*spriteSize,i*spriteSize)
+					add_child(newwall)
+				"*":
+					var newwall = flower.instance()
 					newwall.position = Vector2(j*spriteSize,i*spriteSize)
 					add_child(newwall)
 					

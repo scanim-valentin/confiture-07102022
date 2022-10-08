5e2d36bbf53d364a1acb239755cbd7981a937524
scanim-valentin <guerbett@etud.insa-toulouse.fr>
2022-10-08 13:29:59 +0200

    mdoified terrain element architechture

--- INSTDT/Scripts/Terrain.gd
+++ INSTDT/Scripts/Terrain.gd
@@ -32,18 +32,22 @@ export(PackedScene) var flower
 export(PackedScene) var tree
 export(PackedScene) var dust
 
-
+#Useless for now
+class Bloc :
+	var Element 
+	var Damage: int
+	var ExplosionRange: int
 
 var spriteSize = 48 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
+	randomize()
 	var generationDict = {"#": wall, "*":flower, "o":dust, "t":tree, "~": breakable_wall}
-	
+	#Generating terrain	
 	for i in range(terrainArray.size()) :
 		for j in terrainArray[i].length() :
 			if(terrainArray[i][j] != " ") :
-				print(generationDict[ terrainArray[i][j] ]) 
+				#print(generationDict[ terrainArray[i][j] ]) 
 				var newwall = generationDict[ terrainArray[i][j] ].instance()
 				newwall.position = Vector2(j*spriteSize,i*spriteSize)
 				add_child(newwall)
---
+++ INSTDT/Scripts/TerrainElement.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+export(int) var damage
+export(int) var perk
+export(int) var explosion_area
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
